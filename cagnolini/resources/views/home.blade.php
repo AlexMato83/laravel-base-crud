709c16334df56ddae902069470a9116319f233b5
AlexMato <matontialessandro0@gmail.com>
2020-06-16 23:17:30 +0200

    basilar graphic improvement2

--- cagnolini/resources/views/home.blade.php
+++ cagnolini/resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('main')
   <div class="main">
+    <h2>Visiona i nostri amici pelosi:</h2>
      @foreach ($cagnoliniDb as $cane)
        <a href="{{route('cane', $cane['id'])}}">{{$cane["nome"]}}</a>
 
