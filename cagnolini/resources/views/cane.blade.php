5ad96a161965a5d1aa5fa7c6add69f3468e37a67
AlexMato <matontialessandro0@gmail.com>
2020-06-16 23:10:20 +0200

    basilar graphic improvement

--- cagnolini/resources/views/cane.blade.php
+++ cagnolini/resources/views/cane.blade.php
@@ -2,13 +2,15 @@
 
 @section('main')
 
-     
+     <div class="datiCanePadre">
        <div class="main2">
-       {{$cane["nome"]}}<br>
-       {{$cane["tipo"]}}<br>
-       {{$cane["altezza"]}}<br>
-       {{$cane["peso"]}}
+       <p>nome: <h1>{{$cane["nome"]}}</h1></p><br>
+       <p>razza: {{$cane["tipo"]}}</p><br>
+       <p>altezza: {{$cane["altezza"]}}</p><br>
+       <p>peso: {{$cane["peso"]}}</p>
        </div>
+     </div>
+
 
 
 @endsection
